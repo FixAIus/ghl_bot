7841aac07c9c1619af14b20cde2a4e1c7f2daba8
FixAIus <fixaius@gmail.com>
2024-12-14 01:59:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,11 @@ def index():
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
+@app.route('/firstTest')
+def test():
+    data = request.json
+    return jsonify({"the data": data})
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
