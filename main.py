d9e3c214223cd7ff61973ef4848d5408562c4bbd
FixAIus <fixaius@gmail.com>
2024-12-15 11:56:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from flask import Flask, jsonify, request
+import time
+import asyncio
 import os
 
 app = Flask(__name__)
-
-deez = os.getenv("tezt")
 
 @app.route('/')
 def index():
@@ -16,5 +16,41 @@ def test():
     return jsonify({"the data": deez, "also": data})
 
 
+def upsert_variable(name, value):
+    mutation = f"""
+    mutation {{
+      variableUpsert(
+        input: {{
+          projectId: "{PROJECT_ID}"
+          environmentId: "{ENVIRONMENT_ID}"
+          serviceId: "{SERVICE_ID}"
+          name: "{name}"
+          value: "{value}"
+        }}
+      )
+    }}
+    """
+    response = requests.post(API_URL, headers=HEADERS, json={"query": mutation})
+    if response.status_code != 200:
+        print(f"Failed to upsert variable: {name}. Status Code: {response.status_code}")
+
+
+        
+async def resetKeys():
+    error = False
+    while(!error):
+        try:
+            token = os.getenv("token")
+            refresh = os.getenv("refresh")
+            new_token = token + refresh
+            new_refresh = reftesh + 1
+            upsert_variable("refresh", new_refresh)
+            upsert_variable("token", new_token)
+        except Exception e:
+            #handle errors
+            error = True
+        
+
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
+    resetKeys()
